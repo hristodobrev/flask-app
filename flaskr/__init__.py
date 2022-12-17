cd90087519f51d71609584c1f0b0b03813ed88d4
Hristo Dobrev <hristo.nankov1@gmail.com>
2022-12-17 16:11:11 +0200

    Added blog crud operations.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -13,7 +14,7 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-    
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -28,5 +29,9 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,14 +9,16 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-    
+
     if user_id is None:
         g.user = None
     else:
         g.user = get_db().execute("SELECT * FROM user WHERE id = (?)", (user_id,)).fetchone()
+
 
 def login_required(view):
     @functools.wraps(view)
@@ -27,6 +29,7 @@ def login_required(view):
         return view(**kwargs)
 
     return wrapped_view
+
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -57,6 +60,7 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -64,7 +68,8 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute("SELECT * FROM user WHERE username = (?)",(username,)).fetchone()
+        user = db.execute(
+            "SELECT * FROM user WHERE username = (?)", (username,)).fetchone()
 
         if user is None:
             error = 'Incorrect username'
@@ -80,7 +85,8 @@ def login():
 
     return render_template('auth/login.html')
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('idex'))
+    return redirect(url_for('index'))
